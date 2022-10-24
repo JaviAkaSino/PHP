501e35b2560cd33500f5bb67cd81e11f49a7b64d
JaviAkaSino <javiakasino@hotmail.com>
2022-10-24 21:34:06 +0200

    ej 4 tabla vacia

--- examen_21-22/ejercicio4.php
+++ examen_21-22/ejercicio4.php
@@ -1,4 +1,35 @@
 <?php
+
+function texto_array($separador, $texto)
+{
+
+    $lista = [];
+    $i = 0;
+    $palabra = "";
+
+    while (isset($texto[$i])) {
+
+        if ($texto[$i] != $separador) {
+
+            $palabra .= $texto[$i];
+        } else {
+            if ($palabra != "") {
+
+                $lista[] = $palabra;
+                $palabra = "";
+            }
+        }
+
+        $i++;
+    }
+
+    if ($palabra != "")
+        $lista[] = $palabra; //Ultima palabra
+
+    return $lista;
+}
+
+
 if (isset($_POST["boton_submit"])) {
 
     $error_form = $_FILES["archivo"]["name"] == "" || $_FILES["archivo"]["error"] || $_FILES["archivo"]["type"] != "text/plain" ||  $_FILES["archivo"]["size"] > 1000000;
@@ -6,11 +37,24 @@ if (isset($_POST["boton_submit"])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <title>Horario - Javier Parodi</title>
+    <style>
+        table,
+        td,
+        th {
+            border: 1px solid black;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        th {
+            background-color: #bbb;
+        }
+    </style>
 </head>
 
 <body>
@@ -63,24 +107,154 @@ if (isset($_POST["boton_submit"])) {
         ?>
 
         <h2>Horario de los Profesores</h2>
+        <p>
         <form action="ejercicio4.php" method="post" enctype="multipart/form-data">
             <label for='profesor'>Horario del Profesor: </label>
             <select id='profesor' name='profesor'>
                 <?php
                 while ($linea = fgets($file)) {
-                    $fila_datos = explode("\t", $linea);
+                    $fila_datos = texto_array("\t", $linea);
+                    $profesor = $fila_datos[0];
                     if (isset($_POST["profesor"]) && $_POST["profesor"] == $fila_datos[0]) {
-                        $profesor = $fila_datos[0];
                         echo "<option selected value='" . $profesor . "'>" . $profesor . "</option>";
                     } else
-                    echo "<option value='" . $profesor . "'>" . $profesor . "</option>";
+                        echo "<option value='" . $profesor . "'>" . $profesor . "</option>";
                 }
                 ?>
             </select>
             <button type="submit" name="boton_horario">Ver Horario</button>
         </form>
+        </p>
+        <p>Horario del profesor:
+            <?php echo $_POST["profesor"]; ?>
+        </p>
+        <p>
+        <table>
+            <tr>
+                <th></th>
+                <th>Lunes</th>
+                <th>Martes</th>
+                <th>Miércoles</th>
+                <th>Jueves</th>
+                <th>Viernes</th>
+            </tr>
+            <tr>
+                <th>8:15 - 9:15</th>
+                <td>
 
+                </td>
+                <td>
 
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <th>9:15 - 10:15</th>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <th>10:15 - 11:15</th>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <th>11:15 - 11:45</th>
+                <td colspan="5">
+                    RECREO
+                </td>
+
+            </tr>
+            <tr>
+                <th>11:45 - 12:45</th>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <th>12:45 - 13:45</th>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <th>13:45 - 14:45</th>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        </table>
+        </p>
     <?php
 
 
