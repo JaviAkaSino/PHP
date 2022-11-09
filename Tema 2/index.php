6beace11e942adf9e01ce6d1bf5825e5eaf52563
JaviAkaSino <javiakasino@hotmail.com>
2022-11-09 12:44:00 +0100

    teoria

--- Tema 2/index.php
+++ Tema 2/index.php
@@ -4,6 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Teoría acceso a BD</title>
+    <style>
+        table, th, td{
+            border: 1px solid black;         
+        }
+        table{
+            width: 80%;
+            border-collapse: collapse;
+            text-align: center;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 
 <body>
@@ -17,20 +28,64 @@
         die("Imposible conectar; Error nº " . mysqli_connect_errno() . ": " . mysqli_connect_error());
     }
 
-    $consulta = "SELECT * FROM t_alumnos";
+    $consulta = "SELECT * FROM t_alumnos"; //Creamos la consulta SQL
 
     try {
-        $resultado = mysqli_query($conexion, $consulta);
+        $resultado = mysqli_query($conexion, $consulta); //Crea la query con la conexión y la consulta
+        //var_dump($resultado);
 
-        //Cuando hago un select, puedo obtener o no tuplas, por lo que hay que limpiar para liberar espacio
+        echo "<p><strong>Nº de tuplas obtenidas: </strong>".mysqli_num_rows($resultado)."</p>";
 
-        mysqli_free_result($resultado);
+        $tupla = mysqli_fetch_row($resultado); //Guarda en un array una fila
+        var_dump($tupla);
+        echo "<p><strong>Nombre: </strong>".$tupla[1]."</p>";
+
+        $tupla = mysqli_fetch_assoc($resultado); //Igual pero asociativa
+        var_dump($tupla);
+        echo "<p><strong>Nombre: </strong>".$tupla["nombre"]."</p>";
+
+        $tupla = mysqli_fetch_array($resultado); //Trae ambas
+        var_dump($tupla);
+        echo "<p><strong>Nombre: </strong>".$tupla[1]."</p>";
+        echo "<p><strong>Nombre: </strong>".$tupla["nombre"]."</p>";
+
+        $tupla = mysqli_fetch_object($resultado); //Crea un objeto con tantas props como columnas
+        var_dump($tupla);
+        echo "<p><strong>Nombre: </strong>".$tupla->nombre."</p>";
+
+        mysqli_data_seek($resultado, 0); //Vuelve al índice que le pasemos
+
+        echo "<table>";
+
+            echo "<tr>
+                    <th>Código Alumno</th>
+                    <th>Nombre</th>
+                    <th>Teléfono</th>
+                    <th>Código Postal</th>
+                </tr>";
+
+            while ($tupla = mysqli_fetch_assoc($resultado)){
+
+                echo "<tr>";
+                    echo "<td>".$tupla["cod_alu"]."</td>";
+                    echo "<td>".$tupla["nombre"]."</td>";
+                    echo "<td>".$tupla["telefono"]."</td>";
+                    echo "<td>".$tupla["cp"]."</td>";
+                echo "</tr>";
+            }
+
+        echo "</table>";
 
 
-        mysqli_close($conexion);
+        mysqli_free_result($resultado); //Liberamos espacio por usar consulta
+        mysqli_close($conexion); //Y cerramos conexión
+
     } catch (Exception $e) {
+        //Guarda mensaje
         $mensaje = "Imposible conectar; Error nº " . mysqli_errno($conexion) . ": " . mysqli_error($conexion);
-        mysqli_close($conexion);
+        
+        mysqli_close($conexion); //Cierra conexión
+
         die($mensaje);
     }
 
