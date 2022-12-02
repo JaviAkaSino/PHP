39957e0145cfaab3a93e5fdcc18000d130909ba5
JaviAkaSino <javiakasino@hotmail.com>
2022-12-02 10:16:45 +0100

    fin examen 21

--- Tema 2/Examen_21_22/index.php
+++ Tema 2/Examen_21_22/index.php
@@ -41,6 +41,38 @@
     }
 
 
+//AÑADIR GRUPO
+
+if (isset($_POST["boton_add"])) {
+
+    try { //Lista de los usuarios
+        $consulta = "INSERT INTO horario_lectivo (usuario, dia, hora, grupo, aula) VALUES ('".$_POST["profesor"]."', '".$_POST["dia"]."', '".$_POST["hora"]."', '".$_POST["grupo"]."', 'AÑADIDA')";
+        mysqli_query($conexion, $consulta);
+        $mensaje_accion = "Grupo añadido con éxito";
+    } catch (Exception $e) {
+        $mensaje = "<p>No ha sido posible añadir el grupo. Error Nº" . mysqli_errno($conexion) . " :" . mysqli_error($conexion) . "</p></body></html>";
+        mysqli_close($conexion);
+        die($consulta);
+    }
+}
+
+
+
+    //BORRAR GRUPO
+
+    if (isset($_POST["boton_borrar"])) {
+
+        try { //Lista de los usuarios
+            $consulta = "DELETE FROM horario_lectivo WHERE id_horario = '" . $_POST["boton_borrar"] . "'";
+            mysqli_query($conexion, $consulta);
+            $mensaje_accion = "Grupo eliminado con éxito";
+        } catch (Exception $e) {
+            $mensaje = "<p>No ha sido posible realizar la consulta. Error Nº" . mysqli_errno($conexion) . ":" . mysqli_error($conexion) . "</p></body></html>";
+            mysqli_close($conexion);
+            die($mensaje);
+        }
+    }
+
     try { //Lista de los usuarios
         $consulta = "SELECT id_usuario, nombre FROM usuarios";
         $resultado = mysqli_query($conexion, $consulta);
@@ -79,7 +111,7 @@
 
     <?php
 
-        if (isset($_POST["boton_ver_horario"]) || isset($_POST["boton_editar"])) { //Cuando se pulse el boton ver horario
+        if (isset($_POST["boton_ver_horario"]) || isset($_POST["boton_editar"]) || isset($_POST["boton_borrar"]) || isset($_POST["boton_add"])) { //Cuando se pulse el boton ver horario
 
             echo "<h2>Horario del profesor: " . $nombre_profesor . "</h2>";
 
@@ -165,7 +197,7 @@
         }
 
         //Una vez terminada la tabla
-        if (isset($_POST["boton_editar"])) {
+        if (isset($_POST["boton_editar"]) || isset($_POST["boton_borrar"]) || isset($_POST["boton_add"])) {
 
             //Sacamos la hora (1ª, 2ª...)
             $numero_hora = $_POST["hora"];
@@ -179,16 +211,77 @@
             echo "<h2>Editando la " . $numero_hora . "ª hora (" . $rango_hora . ") del " . $dias[$_POST["dia"]] . "</h2>";
 
             //MENSAJE DE ACCIÓN
-            echo $mensaje_accion;
+            if (isset($mensaje_accion))
+                echo "<p>" . $mensaje_accion . "</p>";
+
+
+            //DATOS DE LA TABLA DE EDICION
+            try {
+                $consulta = "SELECT grupos.nombre, grupos.id_grupo, horario_lectivo.id_horario
+                                FROM grupos, horario_lectivo
+                                WHERE horario_lectivo.grupo = grupos.id_grupo
+                                    AND horario_lectivo.usuario = '" . $_POST["profesor"] . "'
+                                    AND horario_lectivo.dia = '" . $_POST["dia"] . "'
+                                    AND horario_lectivo.hora = '" . $_POST["hora"] . "'";
+
+                $resultado = mysqli_query($conexion, $consulta);
+            } catch (Exception $e) {
+                $mensaje = "<p>Imposible realizar la consulta. Error Nº" . mysqli_errno($conexion) . ":" . mysqli_error($conexion) . "</p></body></html>";
+                mysqli_close($conexion);
+                die($mensaje);
+            }
+
+            $grupos = []; //Guardaremos aquí los id de los grupos para la tabla añadir
 
             //TABLA DE EDICIÓN
             echo "<table>";
-                echo "<tr><th>Grupo</th><th>Acción</th></tr>";
+            echo "<tr><th>Grupo</th><th>Acción</th></tr>";
 
-            
+            while ($tupla = mysqli_fetch_assoc($resultado)) {
+                $grupos[] = $tupla["id_grupo"]; //Guardamos cada grupo
 
+                echo "<tr>";
+                echo "<td>" . $tupla["nombre"] . "</td>";
+                echo "<td>
+                            <form action='index.php' method='post'>
+                                <button name='boton_borrar' value='" . $tupla["id_horario"] . "' type='submit'>Borrar</button>
+                                <input type='hidden' name='profesor' value='" . $_POST["profesor"] . "'/>
+                                <input type='hidden' name='dia' value='" . $_POST["dia"] . "'/>
+                                <input type='hidden' name='hora' value='" . $_POST["hora"] . "'/>              
+                            </form>    
+                        </td>";
+                echo "</tr>";
+            }
             echo "</table>";
 
+            //DATOS DEL SELECT DEL FINAL CON LOS GRUPOS
+            try {
+                $clausula_where = "";
+                if (count($grupos) > 0) //Si hay algún grupo
+                    $clausula_where = " WHERE id_grupo NOT IN (" . implode(", ", $grupos) . ")";
+                //GRUPOS POSIBLES DE AÑADIR
+                $consulta = "SELECT * FROM grupos " . $clausula_where;
+                $resultado = mysqli_query($conexion, $consulta);
+            } catch (Exception $e) {
+                $mensaje = "<p>No ha sido posible realizar la consulta. Error Nº" . mysqli_errno($conexion) . ":" . mysqli_error($conexion) . "</p></body></html>";
+                mysqli_close($conexion);
+                die($mensaje);
+            }
+
+            if (mysqli_num_rows($resultado) > 0) { //  Si el profesor no tiene ya todos los grupos (casi imposible)
+
+                echo "<form action='index.php' method='post'><p>";
+                echo "<select name='grupo'>";
+                    while( $tupla = mysqli_fetch_assoc($resultado))
+                    echo "<option value='".$tupla["id_grupo"]."'>".$tupla["nombre"]."</option>";
+
+                echo "</select>";
+                echo "<button name='boton_add' type='submit'>Añadir</button>";
+                echo "<input type='hidden' name='profesor' value='" . $_POST["profesor"] . "'/>";
+                echo "<input type='hidden' name='dia' value='" . $_POST["dia"] . "'/>";
+                echo "<input type='hidden' name='hora' value='" . $_POST["hora"] . "'/>";
+                echo "</p></form>";
+            }
         }
     } //Haya resultados o no, tras operar liberamos y cerramos
     mysqli_free_result($resultado);
