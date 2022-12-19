6c7d490ab3c5e63d0f3f445b378e18eac35dceb0
JaviAkaSino <javiakasino@hotmail.com>
2022-12-19 12:57:59 +0100

    paginacion

--- Tema 3/Foro/vistas/vista_admin.php
+++ Tema 3/Foro/vistas/vista_admin.php
@@ -47,6 +47,8 @@ if (isset($_POST["boton_confirmar_nuevo"])) {
             mysqli_query($conexion, $consulta);
 
             $_SESSION["mensaje_accion"] = "Usuario registrado con éxito";
+            $_SESSION["buscar"] = $_POST["nombre"];
+            $_SESSION["pagina"] = 1;
             mysqli_close($conexion);
             header("Location:index.php");
             exit;
@@ -60,12 +62,13 @@ if (isset($_POST["boton_confirmar_nuevo"])) {
 }
 
 
-if(isset($_POST["boton_confirmar_borrar"])){
+if (isset($_POST["boton_confirmar_borrar"])) {
 
     try {
-        $consulta = "DELETE FROM usuarios WHERE id_usuario = '".$_POST["boton_confirmar_borrar"]."'";
+        $consulta = "DELETE FROM usuarios WHERE id_usuario = '" . $_POST["boton_confirmar_borrar"] . "'";
         mysqli_query($conexion, $consulta);
         $_SESSION["mensaje_accion"] = "Usuario borrado con éxito";
+        $_SESSION["pagina"] = 1;
         mysqli_close($conexion);
         header("Location:index.php");
         exit;
@@ -114,12 +117,26 @@ if(isset($_POST["boton_confirmar_borrar"])){
         .enlinea {
             display: inline
         }
+
+        .centrar {
+            width: 80%;
+            margin: 1rem auto;
+        }
+
+        .centrar-texto{
+            text-align:center;
+        }
+
+        .flexible {
+            display: flex;
+            justify-content: space-between;
+        }
     </style>
 </head>
 
 <body>
-    <h1>Primer Login - ADMINISTRADOR</h1>
-    <div>
+    <h1 class="centrar-texto">Primer Login - ADMINISTRADOR</h1>
+    <div class="centrar">
         Bienvenido <strong><?php echo $datos_usuario_log["usuario"]; ?></strong> -
         <form class="enlinea" action="index.php" method="post">
             <button class='enlace' name='btnSalir'>Salir</button>
@@ -127,17 +144,70 @@ if(isset($_POST["boton_confirmar_borrar"])){
     </div>
 
 
-    <h2>Listado de usuarios</h2>
+    <h2 class="centrar">Listado de usuarios</h2>
 
     <?php
+    //PAGINACIÓN
+    if (!isset($_SESSION["regs_x_pag"])) {
+        $_SESSION["regs_x_pag"] = 1;
+        $_SESSION["buscar"] = "";
+    }
 
-    $consulta = "SELECT nombre, id_usuario FROM usuarios WHERE tipo = 'normal'";
+    if (isset($_POST["regs_x_pag"])) {
+        $_SESSION["regs_x_pag"] = $_POST["regs_x_pag"];
+        $_SESSION["buscar"] = $_POST["buscar"];
+        $_SESSION["pagina"] = 1;
+    }
+
+
+    if (!isset($_SESSION["pagina"]))
+        $_SESSION["pagina"] = 1;
+
+    if (isset($_POST["pagina"]))
+        $_SESSION["pagina"] = $_POST["pagina"];
+
+    $inicio = ($_SESSION["pagina"] - 1) * $_SESSION["regs_x_pag"];
+
+
+    ?>
+
+    <form class='centrar flexible' method='post' action='index.php'>
+        <div>
+            <label for="regs_x_pag">Registros a mostrar:</label>
+            <select name="regs_x_pag" id="regs_x_pag" onchange="document.getElementById('boton_buscar').click();">
+                <option value="2" <?php if ($_SESSION["regs_x_pag"] == 3) echo "selected" ?>>2</option>
+                <option value="4" <?php if ($_SESSION["regs_x_pag"] == 4) echo "selected" ?>>4</option>
+                <option value="-1" <?php if ($_SESSION["regs_x_pag"] == -1) echo "selected" ?>>ALL</option>
+            </select>
+        </div>
+        <div>
+            <input type="text" name="buscar" value="<?php echo $_SESSION["buscar"] ?>" />
+            <button type="submit" id="boton_buscar">Buscar</button>
+        </div>
+    </form>
+
+    <?php
+    if ($_SESSION["regs_x_pag"] == -1) { //Si es ALL, no limit
+
+        $consulta = "SELECT nombre, id_usuario 
+                        FROM usuarios 
+                        WHERE tipo = 'normal'
+                        AND nombre LIKE '%" . $_SESSION["buscar"] . "%'";
+    } else {
+
+        $consulta = "SELECT nombre, id_usuario 
+                        FROM usuarios 
+                        WHERE tipo = 'normal'
+                        AND nombre LIKE '%" . $_SESSION["buscar"] . "%'
+                        LIMIT " . $inicio . "," . $_SESSION["regs_x_pag"];
+    }
+
 
     try {
 
         $resultado = mysqli_query($conexion, $consulta);
 
-        echo "<table>";
+        echo "<table class='centrar'>";
 
         echo "<tr>
                 <th>Nombre de Usuario <form action='index.php' method='post'><button type='submit' name='boton_nuevo'>[ + ]</button></form></th>
@@ -173,7 +243,6 @@ if(isset($_POST["boton_confirmar_borrar"])){
         }
 
         echo "</table>";
-        mysqli_free_result($resultado);
     } catch (Exception $e) {
 
         $mensaje = "Imposible realizar la consulta. Error Nº " . mysqli_errno($conexion) . ": " . mysqli_error($conexion);
@@ -181,6 +250,54 @@ if(isset($_POST["boton_confirmar_borrar"])){
         session_destroy();
         die($mensaje);
     }
+
+    //BOTONES DEL NÚMERO DE PÁGINA
+
+    if ($_SESSION["regs_x_pag"] != -1) { //Si se muestran ALL, no hay botones
+
+        try { //Sacamos el total de registros
+            $consulta = "SELECT * FROM usuarios WHERE nombre LIKE '%" . $_SESSION["buscar"] . "%'";
+            $resultado = mysqli_query($conexion, $consulta);
+            $numero_registros = mysqli_num_rows($resultado);
+
+            $numero_paginas = ceil($numero_registros / $_SESSION["regs_x_pag"]); //Total de los registros entre los regs por pag
+
+            if ($numero_paginas > 1) { //Si hay mas de una pagina ponemos botones
+
+                echo "<form method='post' action='index.php' class='centrar'>";
+
+                if ($_SESSION["pagina"] > 1) { //Si no estamos en la 1, aparecen flechas pabajo
+                    echo "<button type='submit' name='pagina' value='1'>|<</button>";
+                    echo "<button type='submit' name='pagina' value='" . ($_SESSION["pagina"] - 1) . "'><</button>";
+                }
+
+                for ($i = 1; $i <= $numero_paginas; $i++) {
+
+                    if ($i == $_SESSION["pagina"]) //Desactiva el boton de la página en la que está
+                        echo "<button disabled type='submit' name='pagina' value = '" . $i . "'>" . $i . "</button>";
+                    else
+                        echo "<button type='submit' name='pagina' value = '" . $i . "'>" . $i . "</button>";
+                }
+
+                if ($_SESSION["pagina"] < $numero_paginas) { //Si no estamos en la última, aparecen flechas hacia delante
+                    echo "<button type='submit' name='pagina' value='" . ($_SESSION["pagina"] + 1) . "'>></button>";
+                    echo "<button type='submit' name='pagina' value='" . $numero_paginas . "'>>|</button>";
+                }
+                echo "</form>";
+            }
+        } catch (Exception $e) {
+
+            $mensaje = "Imposible realizar la consulta. Error Nº " . mysqli_errno($conexion) . ": " . mysqli_error($conexion);
+            mysqli_close($conexion);
+            session_destroy();
+            die($mensaje);
+        }
+    }
+
+    mysqli_free_result($resultado);
+
+    /**** FIN TABLA ****/
+
 
     if (isset($_SESSION["mensaje_accion"])) {
 
@@ -195,9 +312,11 @@ if(isset($_POST["boton_confirmar_borrar"])){
             $consulta = "SELECT * FROM usuarios WHERE id_usuario = '" . $_POST["boton_listar"] . "'";
             $resultado = mysqli_query($conexion, $consulta);
 
+            echo "<div class='centrar'>";
             if (mysqli_num_rows($resultado) > 0) { //Si el usuario sigue existiendo
 
                 $tupla = mysqli_fetch_assoc($resultado);
+
                 echo "<h3>Datos del usuario " . $_POST["boton_listar"] . "</h3>";
                 echo "<p><strong>Nombre: </strong>" . $tupla["nombre"] . "</p>";
                 echo "<p><strong>Usuario: </strong>" . $tupla["usuario"] . "</p>";
@@ -208,6 +327,8 @@ if(isset($_POST["boton_confirmar_borrar"])){
                 echo "<p class ='error'>Error de consistencia. El usuario seleccionado ya no existe</p>";
             }
 
+            echo "</div>";
+
             mysqli_free_result($resultado);
         } catch (Exception $e) {
 
@@ -217,6 +338,9 @@ if(isset($_POST["boton_confirmar_borrar"])){
             die($mensaje);
         }
     }
+
+
+    /************  NUEVO USUARIO  ***************/
 
     if (isset($_POST["boton_nuevo"]) || isset($_POST["boton_confirmar_nuevo"])) {
 
@@ -245,7 +369,7 @@ if(isset($_POST["boton_confirmar_borrar"])){
 
             <p>
                 <label for="clave">Contraseña: </label>
-                <input type="text" id="clave" name="clave" value="<?php if (isset($_POST["clave"])) echo $_POST["clave"]; ?>">
+                <input type="password" id="clave" name="clave" value="<?php if (isset($_POST["clave"])) echo $_POST["clave"]; ?>">
                 <?php if (isset($_POST["clave"]) && $error_clave)
                     echo "<span class='error'>* Campo vacío * </span>"; ?>
             </p>
@@ -278,11 +402,11 @@ if(isset($_POST["boton_confirmar_borrar"])){
 
     if (isset($_POST["boton_borrar"])) {
 
-        echo "<h3>Borrar usuario</h3>";
-        echo "<p>¿Desea borrar el usuario ".$_POST["boton_borrar"]."?</p>";
-        echo "<form action='index.php' method='post'>
+        echo "<h3 class='centrar'>Borrar usuario</h3>";
+        echo "<p class='centrar'>¿Desea borrar el usuario " . $_POST["boton_borrar"] . "?</p>";
+        echo "<form class='centrar' action='index.php' method='post'>
                 <button type='submit'>Atras</button>
-                <button type='submit' name='boton_confirmar_borrar' value='".$_POST["boton_borrar"]."'>Confirmar</button>
+                <button type='submit' name='boton_confirmar_borrar' value='" . $_POST["boton_borrar"] . "'>Confirmar</button>
             </form>";
     }
 
