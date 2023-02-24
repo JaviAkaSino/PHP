55417ec9ecb0b7932e98329c65995426bd867a2f
JaviAkaSino <javiakasino@hotmail.com>
2023-02-24 10:01:45 +0100

    clase

--- Tema_6/Practica2/js/index.js
+++ Tema_6/Practica2/js/index.js
@@ -1,8 +1,3 @@
-const DIR_SERV = "http://localhost/PHP/Tema_5/Miguel/Ejercicio1/servicios_rest_ejer1";
-
-$(document).ready(function () {
-    obtener_productos();
-})
 
 function volver() {
     $("div#respuestas").html("");
---
+++ Tema_6/Practica2/js/login.js
@@ -0,0 +1,6 @@
+const DIR_SERV = "http://localhost/PHP/Tema_5/Miguel/Ejercicio1_key/servicios_rest_ejer1";
+
+
+$(document).ready(function () {
+    obtener_productos();
+})
